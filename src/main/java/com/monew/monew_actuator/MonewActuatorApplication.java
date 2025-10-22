914e57d847aa2dd67dc6b25d8b1cfcd3d40dda4a
jeonggiju <halogiju123@gmail.com>
2025-10-22 11:03:51 +0900

    프로젝트 초기 코드 추가
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/monew/monew_actuator/MonewActuatorApplication.java
@@ -0,0 +1,15 @@
+package com.monew.monew_actuator;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MonewActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonewActuatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monew/monew_actuator/MonewActuatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.monew.monew_actuator;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@EnableAdminServer
+class MonewActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
